cf14cfe884aa10741c8a449d5086c99df1909874
Web Developer <74357604+sumairmobeen@users.noreply.github.com>
2021-01-27 19:28:02 +0500

    676

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 
 
-// var url = "https://my-chat1.herokuapp.com";
-var url = "http://localhost:5000";
+var url = "https://my-chat1.herokuapp.com";
+// var url = "http://localhost:5000";
 var socket = io(url);
 
 socket.on('connect', function () {
@@ -71,6 +71,7 @@ function getProfile() {
         document.getElementById('name').innerHTML = response.data.profile.name
         document.getElementById('email').innerHTML = response.data.profile.email
         document.getElementById("show_pic").src = response.data.profile.profilePic
+        sessionStorage.setItem('email', response.data.profile.email)
     }, (error) => {
         console.log(error.message);
     });
@@ -83,33 +84,14 @@ function upload() {
 
     var fileInput = document.getElementById("fileInput");
 
-    // // To convert a File into Blob (not recommended)
-    // var blob = null;
-    // var file = fileInput.files[0];
-    // let reader = new FileReader();
-    // reader.readAsArrayBuffer(file)
-    // reader.onload = function (e) {
-    //     blob = new Blob([new Uint8Array(e.target.result)], { type: file.type });
-    //     console.log(blob);
-    // }
-
     console.log("fileInput: ", fileInput);
     console.log("fileInput: ", fileInput.files[0]);
 
     let formData = new FormData();
-    // https://developer.mozilla.org/en-US/docs/Web/API/FormData/append#syntax
 
-    formData.append("myFile", fileInput.files[0]); // file input is for browser only, use fs to read file in nodejs client
-    // formData.append("myFile", blob, "myFileNameAbc"); // you can also send file in Blob form (but you really dont need to covert a File into blob since it is Actually same, Blob is just a new implementation and nothing else, and most of the time (as of january 2021) when someone function says I accept Blob it means File or Blob) see: https://stackoverflow.com/questions/33855167/convert-data-file-to-blob
-    formData.append("email", sessionStorage.getItem("email")); // this is how you add some text data along with file
-    formData.append("myDetails",
-        JSON.stringify({
-            "subject": "Science",   // this is how you send a json object along with file, you need to stringify (ofcourse you need to parse it back to JSON on server) your json Object since append method only allows either USVString or Blob(File is subclass of blob so File is also allowed)
-            "year": "2021"
-        })
-    );
+    formData.append("myFile", fileInput.files[0]); 
+    formData.append("email", sessionStorage.getItem("email")); 
 
-    // you may use any other library to send from-data request to server, I used axios for no specific reason, I used it just because I'm using it these days, earlier I was using npm request module but last week it get fully depricated, such a bad news.
     axios({
         method: 'post',
         url: url + "/upload",
@@ -120,8 +102,6 @@ function upload() {
             console.log(`  upload Success`);
             alert("upload Success")
             // document.getElementById("show_pic").innerHTML = instanceOfFileReader.readAsDataURL(res.data);
-
-
         })
         .catch(err => {
             console.log(err);
@@ -213,7 +193,9 @@ function gettweet() {
         let tweets = response.data.gettweet;
         for (i = 0; i < tweets.length; i++) {
             var eachtweet = document.createElement("li");
-            eachtweet.innerHTML = `<h4>
+            eachtweet.innerHTML = `
+            <img src="${tweets[i].profilePic}" width="50" height="50 " style="border-radius: 100%"/>
+            <h4>
                 ${tweets[i].username}
                 </h4>
                  <p>
@@ -238,7 +220,10 @@ function mytweet() {
         let tweets = response.data.tweet;
         for (i = 0; i < tweets.length; i++) {
             var eachtweet = document.createElement("li");
-            eachtweet.innerHTML = `<h4>
+            eachtweet.innerHTML = `
+            <img src="${tweets[i].profilePic}" width="50" height="50 " style="border-radius: 100%"/>
+            
+            <h4>
                 ${tweets[i].username}
                 </h4>
                  <p>
@@ -249,16 +234,16 @@ function mytweet() {
     }, (error) => {
         console.log(error.message);
     });
+    return false
 }
 
 socket.on("NEW_POST", (newPost) => {
-
-
     console.log(newPost);
-
     let jsonRes = newPost;
     var eachtweet = document.createElement("li");
-    eachtweet.innerHTML = `<h4>
+    eachtweet.innerHTML = `
+    <img src="${jsonRes.profilePic}" width="50" height="50 " style="border-radius: 100%"/>  
+    <h4>
     ${jsonRes.username}
     </h4>
      <p>
@@ -277,7 +262,9 @@ socket.on("MY_POST", (newPost) => {
 
     let jsonRes = newPost;
     var eachtweet = document.createElement("li");
-    eachtweet.innerHTML = `<h4>
+    eachtweet.innerHTML = `
+    <img src="${jsonRes.profilePic}" width="50" height="50 " style="border-radius: 100%"/>
+    <h4>
     ${jsonRes.username}
     </h4>
      <p>
@@ -303,7 +290,7 @@ function previewFile() {
     if (file) {
         reader.readAsDataURL(file);
     }
-}
+}   
 
 function logout() {
     axios({
